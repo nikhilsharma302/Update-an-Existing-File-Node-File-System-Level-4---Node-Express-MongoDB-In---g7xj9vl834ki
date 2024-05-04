56978472feda044870a9884e409bcb322f45def3
NIKHIL SHARMA <nikhilsharma302@gmail.com>
2024-05-04 21:10:11 +0530

    code added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
+  const text=await fs.readFile(fileName);
+  const newtext=text.concat(fileContent);
+  fs.writeFile(fileName,newtext)
   
 };
 
